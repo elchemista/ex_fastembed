1d9fb12792c455d6a8acc66f83febe2b9fad1db5
elchemista <darkyfelis@gmail.com>
2025-05-12 13:30:06 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -52,10 +52,10 @@ defmodule ExFastembed.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:rustler, ">= 0.0.0", optional: true},
+      {:rustler, "~> 0.36.1"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:rustler_precompiled, "~> 0.8"},
+      # {:rustler_precompiled, "~> 0.8"},
       # Documentation Provider
       {:ex_doc, "~> 0.28.3", only: [:dev, :test], optional: true, runtime: false}
     ]

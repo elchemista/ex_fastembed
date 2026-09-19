049f9497230d6b5142aa0ce092101b4318aa2006
Yuriy Zhar <darkyfelis@gmail.com>
2026-09-19 12:30:12 +0200

    Add verified checksums for 0.1.1 NIF assets

---
+++ checksum-Elixir.ExFastembed.Native.exs
@@ -0,0 +1,10 @@
+%{
+  "ex_fastembed-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:88d4b3990e3bd6e56759d073d6b99d7806553c9ddb7feacd81782d3756706ca2",
+  "ex_fastembed-v0.1.1-nif-2.16-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:a5d9b4475ce66b454d787c7126192261c720df9c230a82b8f7ea721c54e4b7ea",
+  "libex_fastembed-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:1e703ec03e471f7d1295d1ef690fb1c0f24563b6e06fc16ef625606cff52da85",
+  "libex_fastembed-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:041b0ff41be9644c052c4d3921cdab4a9506813ae2580ffbbbc7f1134222dfc3",
+  "libex_fastembed-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f3416fdf78bb9dca6b6f5bcc48c003ec4aa202a8c896ccc314415976e08695cb",
+  "libex_fastembed-v0.1.1-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:7c89ba246209d7dcacb6712c6cd8599fbee1394a31969e66521e05aa8aa78d6d",
+  "libex_fastembed-v0.1.1-nif-2.16-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:04ef0cfe0976e84b7c436c3b3478b72dfb7db893c25ee9cacb6e2714194cbb9f",
+  "libex_fastembed-v0.1.1-nif-2.16-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:ee733fc0158462743226a34621e062e5acc74b7df7dc99fe5da8cb0a966dbabb",
+}
